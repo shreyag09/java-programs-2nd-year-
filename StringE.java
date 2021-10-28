e3f24bc86492055f4543ab771a461d42310d1c9d
shreyag09 <81857281+shreyag09@users.noreply.github.com>
2021-10-28 11:13:41 +0530

    Add files via upload

---
+++ StringE.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class StringE
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str1=sc.nextLine();
+
+System.out.println("Upper Case "+str1.toUpperCase());
+System.out.println("Lower Case  "+str1.toLowerCase());
+String str2=str1.toUpperCase();
+System.out.println("original string:  "+str1);
+System.out.println("New:  "+str2);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ StringF.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class StringF
+{
+public static void main(String args[])
+{
+
+String str1="Direct Assignmnet";
+String str2="Direct Assignmnet";
+String str3=new String("Direct Assignmnet");
+String str4=new String("Direct Assignmnet");
+str1=str1+str2;
+str3=str3.concat(str4);
+System.out.println(str1);
+System.out.println(str4);
+}
+}
+
+
+
+
+
+
+
+
+
+
